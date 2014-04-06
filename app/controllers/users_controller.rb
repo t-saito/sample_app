0c2bb7d756469f99ffd10f1ba599b71459c1c8d3
tksh <tksh@tksh-no-MacBook-Pro.local>
2014-04-06 15:52:22 +0900

    tutorial 9.2 finished.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -20,11 +22,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       # 更新に成功した場合を扱う。
       flash[:success] = "Profile updated"
@@ -34,4 +34,18 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
