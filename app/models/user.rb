5447a89fce3e036314f4179fda0e9a8db4a28941
tksh <tksh@tksh-no-MacBook-Pro.local>
2014-08-10 15:56:10 +0900

    10.1.3 finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,10 @@
 #  updated_at :datetime         not null
 #
 
-class User < ActiveRecord::Base 
+class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  has_many :microposts
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
